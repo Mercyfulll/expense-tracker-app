d671dfc195755048999bfa00035269dddc9e7034
Mercyfulll <mercyful.buthelezi@gmail.com>
2023-11-29 12:33:44 +0200

    third commit

--- expensetracker.js
+++ expensetracker.js
@@ -2,6 +2,10 @@ export default function expenseTracker(db){
     async function getAllFromCategory(){
         return await db.manyOrNone(`SELECT * FROM category`)
     } 
+
+    async function addExpense(categoryId, amount){
+         
+    }
     
     return{
         getAllFromCategory
